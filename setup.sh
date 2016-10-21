eed6c50d2cd570fd3fbd258ce760004a7737eeb8
Jonathan Uesato <jonathan@getcruise.com>
2016-10-20 22:06:15 -0700

    Add some files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s $PWD/bashrc ~/.bashrc
+ln -s $PWD/user.sublime-keymap "/home/juesato/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" 
+ln -s $PWD/Preferences.sublime-settings "/home/juesato/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+
