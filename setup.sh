0fab7c38a1c3096c52a779e2fcdf5c1675f824cd
Jonathan Uesato <juesato@mit.edu>
2016-10-25 00:04:29 -0700

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+mkdir -p backup
+mv "/home/juesato/.bashrc" backup/.bashrc
+mv "/home/juesato/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" "backup/Default (Linux).sublime-keymap"
+mv "/home/juesato/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" "backup/Preferences.sublime-settings"
 
 ln -s $PWD/bashrc ~/.bashrc
 ln -s $PWD/user.sublime-keymap "/home/juesato/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" 
